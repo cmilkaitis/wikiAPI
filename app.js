b336f87211490b27b126d56ab6273085fc1e897b
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-23 10:41:13 -0400

    Inline CSS

--- app.js
+++ app.js
@@ -10,18 +10,15 @@ $(document).ready(function() {
        $("#content").empty();
        let term = $("#search-text").val();
        let url = wikiUrl + term;
-
+ 
        $.getJSON(url, function(json){
-           console.log(json);
+           $("#content").html("<h2>Results for your search of " + json[0]);
            for (let i = 0; i < json[1].length; i++) {    
-                $("#content").append("<a id='l" + i +"' href=''><div id='c"+ i +"' class='items'><p>" + json[1][i] + "</p></div></a>");
-            }
-            for (let i = 0; i < json[2].length; i++) {
+                $("#content").append("<a id='l" + i +"' href=''><div id='c"+ i +"' class='items' style='margin-top: 5px; margin-bottom: 5px; border: solid #007bff 1px; padding-left: 5px; padding-top: 5px;'><p>" + json[1][i] + "</p></div></a>");
                 $(`#c${i}`).append("<p>" + json[2][i] + "</p>");
-            } 
-            for (let i = 0; i < json[3].length; i++) {
-                $(`#l${i}`).attr("href", json[3][i]);
-            } 
-       });
+                $(`#l${i}`).attr("href", json[3][i]);  
+            }
+        });
+        $("#search-text").val('');
    });  
 });
