c01ce1861a5ec4c4e6f259a35aee8ba5a5dd7bb1
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-24 10:29:20 -0400

    Quote change

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
        let url = wikiUrl + term;
  
        $.getJSON(url, function(json){
-           $("#content").html("<h2>Results for your search of " + json[0]);
+           $("#content").html("<h2>Results for your search of '" + json[0] + "'");
            for (let i = 0; i < json[1].length; i++) {    
                 $("#content").append("<a id='l" + i +"' href=''><div id='c"+ i +"' class='items' style='margin-top: 5px; margin-bottom: 5px; border: solid #007bff 1px; padding-left: 5px; padding-top: 5px;'><p>" + json[1][i] + "</p></div></a>");
                 $(`#c${i}`).append("<p>" + json[2][i] + "</p>");
