9000861575ee073525506a64d1d6c827633e99fe
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-29 12:13:40 -0400

    Added styles

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&formatversion=2&search="
+    let wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&formatversion=2&search=";
    
     $("#random").click(function() {
         window.open('https://en.wikipedia.org/wiki/Special:Random');
@@ -12,9 +12,9 @@ $(document).ready(function() {
        let url = wikiUrl + term;
  
        $.getJSON(url, function(json){
-           $("#content").html("<h2>Results for your search of '" + json[0] + "'");
+           $("#content").html("<h2 class='text-center' style='margin-top: 10px; font-family: Lobster, cursive;'>Results for your search of '" + json[0] + "'");
            for (let i = 0; i < json[1].length; i++) {    
-                $("#content").append("<a id='l" + i +"' href=''><div id='c"+ i +"' class='items' style='margin-top: 5px; margin-bottom: 5px; border: solid #007bff 1px; padding-left: 5px; padding-top: 5px;'><p>" + json[1][i] + "</p></div></a>");
+                $("#content").append("<a id='l" + i +"' href=''><div id='c"+ i +"' class='items' style='background: white; border-radius: 5px; margin-top: 5px; margin-bottom: 5px; padding: 10px 10px;'><p style='font-family: Lobster, cursive;'>" + json[1][i] + "</p></div></a>");
                 $(`#c${i}`).append("<p>" + json[2][i] + "</p>");
                 $(`#l${i}`).attr("href", json[3][i]);  
             }
